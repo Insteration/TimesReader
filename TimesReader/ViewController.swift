8a677edab8eab2321bf42b8993108394e372e081
Artem <inste@icloud.com>
2019-05-27 13:54:12 +0300

    Initial Commit

---
+++ TimesReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimesReader
+//
+//  Created by Art Karma on 5/27/19.
+//  Copyright © 2019 Art Karma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
